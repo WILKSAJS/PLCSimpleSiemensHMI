ec47c55a7a593ee047f55126d3c660ed36dd458f
Bartek Wilk <wilksajs@gmail.com>
2021-05-07 22:32:13 +0200

    Add working Caliburn - Shell MVVM structure

---
+++ Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Caliburn.Micro;
+using PLCSiemensSymulatorHMI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PLCSiemensSymulatorHMI
+{
+    class Bootstrapper: BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
