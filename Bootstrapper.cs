44836162eead4b5d5151f4f297d7861810730cd4
Bartek Wilk <wilksajs@gmail.com>
2021-05-08 11:50:02 +0200

    Add CaliburnMicro DI Container config to Bootstrapper Class

--- Bootstrapper.cs
+++ Bootstrapper.cs
@@ -11,6 +11,7 @@ namespace PLCSiemensSymulatorHMI
 {
     class Bootstrapper: BootstrapperBase
     {
+        SimpleContainer _container = new SimpleContainer();
         public Bootstrapper()
         {
             Initialize();
@@ -19,6 +20,27 @@ namespace PLCSiemensSymulatorHMI
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override void Configure()
+        {
+            _container.Singleton<IWindowManager, WindowManager>();
+            _container.Singleton<IEventAggregator, EventAggregator>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
         }
     }
 }
