58d3807e824d294e3ca646fa639bb888fefde307
Bartek Wilk <wilksajs@gmail.com>
2021-05-09 16:03:00 +0200

    Add PlcServiec to application and use it methods to perform operations.

--- ViewModels/ShellViewModel.cs
+++ ViewModels/ShellViewModel.cs
@@ -10,13 +10,40 @@ namespace PLCSiemensSymulatorHMI.ViewModels
 {
     public class ShellViewModel: PropertyChangedBase
     {
+        private readonly Sharp7PlcService _plcService;
+        public ShellViewModel()
+        {
+            _plcService = new Sharp7PlcService();
+            // for initialzie purpose
+            OnPlcServiceValueUpdated(null,null);
+            IpAdress = "127.0.0.1";
+            Rack = "0";
+            Slot = "1";
+            _plcService.ValuesUpdated += OnPlcServiceValueUpdated;
+        }
+
+
         #region Properties
         // First method
         private string _ipAdress;
         public string IpAdress
         {
-            get => _ipAdress; 
+            get => _ipAdress;
             set => Set(ref _ipAdress, value);
+        }
+
+        private string _rack;
+        public string Rack
+        {
+            get => _rack; 
+            set => Set(ref _rack, value);
+        }
+
+        private string _slot;
+        public string Slot
+        {
+            get => _slot;
+            set => Set(ref _slot, value);
         }
 
         // Second Method
@@ -72,6 +99,7 @@ namespace PLCSiemensSymulatorHMI.ViewModels
         }
 
         private TimeSpan timeSpan;
+        
 
         public TimeSpan TimeScan
         {
@@ -87,24 +115,38 @@ namespace PLCSiemensSymulatorHMI.ViewModels
         #region Commands
         public void Connect()
         {
+            int rackResult; // default 0
+            int slotResult; // default 1
+            rackResult = Int32.TryParse(Rack, out rackResult) ? rackResult : 0;
+            slotResult = Int32.TryParse(Slot, out slotResult) ? slotResult : 1;
 
+            _plcService.ConnectToPlc(IpAdress, rackResult, slotResult);
         }
         public void Disconnect()
         {
-
+            _plcService.Disconnect();
         }
 
-        public void Start()
+        public async void Start()
         {
-            
+            await _plcService.WriteStartMonostable();
         }
 
-        public void Stop()
+        public async void Stop()
         {
-           
+            await _plcService.WriteStopMonostable();
         }
         #endregion
 
+        private void OnPlcServiceValueUpdated(object sender, EventArgs e)
+        {
+            ConnectionState = _plcService.ConnectionState;
+            PumpState = _plcService.PumpState;
+            HighSensor = _plcService.HighLimit;
+            LowSensor = _plcService.LowLimit;
+            TankLevel = _plcService.TankLevel;
+            TimeScan = _plcService.ScanTtime;
+        }
 
     }
 }
