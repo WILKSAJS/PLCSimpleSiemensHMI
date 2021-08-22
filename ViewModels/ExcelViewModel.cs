3a19b7e18a08121b459518060add2358303dfae7
Bartek Wilk <wilksajs@gmail.com>
2021-08-22 19:38:13 +0200

    Installed nuget packages to read Excel files. Created Excel View and VM and store it in CreateVM  as Bindable list. Added functionality to read ecel files in CreateView

---
+++ ViewModels/ExcelViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PLCSiemensSymulatorHMI.ViewModels
+{
+    public class ExcelViewModel : Screen
+    {
+        private string _controlName;
+        public string ControlName
+        {
+            get => _controlName;
+            set => Set(ref _controlName, value);
+        }
+
+        private string _dataBlock;
+        public string DataBlock
+        {
+            get => _dataBlock;
+            set => Set(ref _dataBlock, value);
+        }
+
+        private string _index;
+        public string Index
+        {
+            get => _index;
+            set => Set(ref _index, value);
+        }
+
+        private string _offset;
+        public string Offset
+        {
+            get => _offset;
+            set => Set(ref _offset, value);
+        }
+    }
+}
